972dbda9d7c3c7a8ed0c4300e7c060da73c4c802
3A532088 <paileo007007@gmail.com>
2018-11-01 18:53:18 +0800

    練習二更正

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::get('student/{student_no}/score', function ($student_no) {
 });
 
 Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
-    return "學號:".$student_no."的".$subject."所有成績";
+    return "學號:".$student_no."的".$subject."成績";
 });
+
