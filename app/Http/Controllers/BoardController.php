14714fc86a0690917ba7b16957c7cc86387dae91
3A532088 <paileo007007@gmail.com>
2018-11-21 19:23:30 +0800

    練習四：DB 存取 & Model 練習 part I

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Score as ScoreEloquent;
+use Route;
+use View;
 
 class BoardController extends Controller
 {
     //
     public function getIndex(){
-        return view('board');
+        return View::make('board',['scores'=>ScoreEloquent::with('student')->orderByTotal()->orderBySubject()->get()]);
+    }
+
+    public function getName(){
+        return Route::currentRouteAction();
     }
 }
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,24 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+
+use App\Student;
+
+use View;
 
 class StudentController extends Controller
 {
 
     public function getStudentData($student_no){
-        return \View::make('score',[
-            'student_no'=>$student_no,
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
             'subject'=>null
         ]);
     }
 
     public function getStudentScore($student_no,$subject = null){
-        return \View::make('score',[
-            'student_no'=>$student_no,
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
             'subject'=>$subject
         ]);
     }
 
 
-    //
 }
