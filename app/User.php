8c1b08c106656bbb505e21c30ed6150ba3027007
3A532088 <paileo007007@gmail.com>
2018-11-21 19:01:08 +0800

    練習三：建立 Model 練習

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use \App\Student as StudentEloquent;
 
 class User extends Authenticatable
 {
@@ -27,4 +28,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function student(){
+        return $this->hasOne(StudentEloquent::class);
+    }
 }
